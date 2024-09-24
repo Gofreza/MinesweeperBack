a6a483a31145a4bfaa16e0fa157977fd66b71ff4
Theo DUSEHU <t.dusehu@cat-amania.com>
2024-09-24 15:58:19 +0200

    init

---
+++ src/main/java/fr/alternants/Multisweeper/MultisweeperApplication.java
@@ -0,0 +1,13 @@
+package fr.alternants.Multisweeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultisweeperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultisweeperApplication.class, args);
+	}
+
+}
