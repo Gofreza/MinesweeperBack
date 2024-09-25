3faf2cc3f216f22fac7e5626d366addbf94e3064
t.dusehu <theo.dusehu@sfr.fr>
2024-09-25 14:55:53 +0200

    Add cross origin bypass

--- src/main/java/fr/alternants/Multisweeper/game/GameController.java
+++ src/main/java/fr/alternants/Multisweeper/game/GameController.java
@@ -2,13 +2,11 @@ package fr.alternants.Multisweeper.game;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/game")
+@CrossOrigin(origins = "http://localhost:5173")
 @RequiredArgsConstructor
 public class GameController {
 
