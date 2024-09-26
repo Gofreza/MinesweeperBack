c10d5a9031f715abcca4439afa25c29d49d2df4e
m.devigne <mathisdevigne8@gmail.com>
2024-09-26 11:32:21 +0200

    Remove main, fix flag

--- src/main/java/fr/alternants/Multisweeper/MultisweeperApplication.java
+++ src/main/java/fr/alternants/Multisweeper/MultisweeperApplication.java
@@ -1,7 +1,5 @@
 package fr.alternants.Multisweeper;
 
-import fr.alternants.Multisweeper.game.core.Difficulty;
-import fr.alternants.Multisweeper.game.core.Multisweeper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,10 +8,6 @@ public class MultisweeperApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MultisweeperApplication.class, args);
-		Multisweeper multisweeper = new Multisweeper(4, 4, false, Difficulty.HARD);
-		System.out.println(multisweeper);
-		multisweeper.play(0, 0);
-		System.out.println(multisweeper);
 	}
 
 }
