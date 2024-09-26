825455be2e2b284425f251f63d7a39172ec262c8
m.devigne <mathisdevigne8@gmail.com>
2024-09-26 11:13:55 +0200

    Add ws basic controller

---
+++ src/main/java/fr/alternants/Multisweeper/config/WebSocketEventListener.java
@@ -0,0 +1,23 @@
+package fr.alternants.Multisweeper.config;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class WebSocketEventListener {
+
+    @EventListener
+    public void handleWebSocketConnectListener(SessionDisconnectEvent event) {
+        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
+        String username = (String) headerAccessor.getSessionAttributes().get("username");
+        if (username != null) {
+            log.info("User Disconnected: " + username);
+        }
+    }
+}
---
+++ src/main/java/fr/alternants/Multisweeper/game/WSGameController.java
@@ -0,0 +1,47 @@
+package fr.alternants.Multisweeper.game;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class WSGameController {
+
+    private final GameService gameService;
+
+    @MessageMapping("/new")
+    @SendTo("/topic/game")
+    public Integer newGame(@Payload NewGameRequest newGameRequest) {
+        System.out.println("New game request: " + newGameRequest);
+        return gameService.newGame(newGameRequest);
+    }
+
+
+    @MessageMapping("/play")
+    @SendTo("/topic/game")
+    public PlayResponse play(@RequestBody PlayRequest playRequest) {
+        System.out.println("Play request: " + playRequest);
+        return gameService.play(playRequest);
+    }
+
+
+    @MessageMapping("/play")
+    @SendTo("/topic/game")
+    public PlayResponse.CellResponse flag(@RequestBody PlayRequest playRequest) {
+        System.out.println("Flag request: " + playRequest);
+        return gameService.flag(playRequest);
+    }
+
+    @MessageMapping("/grid")
+    @SendTo("/topic/game")
+    public List<PlayResponse.CellResponse> getGrid(@RequestBody Integer roomId) {
+        System.out.println("Get grid, roomId: " + roomId);
+        return gameService.getGrid(roomId);
+    }
+}
